e3820bf8405ba815debf97d210623ea21964ba4f
default <default@mail.com>
2017-12-12 16:28:34 +0200

    MakeGrid function basic functionality, predefined rows and columns

--- designs.js
+++ designs.js
@@ -5,6 +5,20 @@
 
 function makeGrid() {
 
-// Your code goes here!
+  // Your code goes here!
+  // Just test variable that will hold rows and colomns
+  let row = 4;
+  let colomns = 5;
 
+  //The jQuery function that will create the grid
+  jQuery(function($) {
+    for (let n = 0; n < row; n++) {
+      $('#pixel_canvas').append('<tr></tr>');
+      for (var m = 0; m < colomns; m++) {
+        $('tr').last().append('<td></td>');
+      }
+    }
+  });
 }
+
+makeGrid();
