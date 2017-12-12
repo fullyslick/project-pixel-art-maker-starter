3e549c2c9994b7fb6e4d8fe4a56d1887ec8bc76a
default <default@mail.com>
2017-12-12 17:14:07 +0200

    Putting all functions inside jQuery document ready

--- designs.js
+++ designs.js
@@ -1,36 +1,33 @@
-//@description Java script function that is called when 'submit' button is clicked
-function makeGrid() {
-  //@description The jQuery function that will create the grid.
   jQuery(function($) {
+    //@description Java script function that is called when 'submit' button is clicked
+    function makeGrid() {
 
-    //@description If there is already Grid created, remove it and create new one.
-    $('tr').remove();
+      //@description If there is already Grid created, remove it and create new one.
+      $('tr').remove();
 
-    //@description Get the input value for height of the Grid.
-    let row = $('#input_height').val();
+      //@description Get the input value for height of the Grid.
+      let row = $('#input_height').val();
 
-    //@description Get the input value for width of the Grid.
-    let colomns = $('#input_width').val();
+      //@description Get the input value for width of the Grid.
+      let colomns = $('#input_width').val();
 
-    //@description Nested loop to create the table taking the input values.
-    for (let n = 0; n < row; n++) {
+      //@description Nested loop to create the table taking the input values.
+      for (let n = 0; n < row; n++) {
 
-      //@description Create table row element.
-      $('#pixel_canvas').append('<tr></tr>');
-      for (var m = 0; m < colomns; m++) {
+        //@description Create table row element.
+        $('#pixel_canvas').append('<tr></tr>');
+        for (let m = 0; m < colomns; m++) {
 
-        //@description Create table cell.
-        $('tr').last().append('<td></td>');
+          //@description Create table cell.
+          $('tr').last().append('<td></td>');
+        }
       }
     }
-  });
-}
 
-jQuery(function($) {
-  //@description Get the form and on click of 'submit' button call makeGrid() function.
-  //@param {event lsitener object} e
-  $('form').submit(function(e) {
-    makeGrid();
-    e.preventDefault();
+    //@description Get the form and on click of 'submit' button call makeGrid() function.
+    //@param {event lsitener object} e
+    $('form').submit(function(e) {
+      makeGrid();
+      e.preventDefault();
+    });
   });
-});
