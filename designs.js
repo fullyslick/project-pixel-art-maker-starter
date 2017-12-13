e1aafc0e53630c63b4ce8b9e19f60e70aa4ebef6
default <default@mail.com>
2017-12-13 16:19:55 +0200

    On click of cell change background

--- designs.js
+++ designs.js
@@ -1,7 +1,6 @@
   jQuery(function($) {
     //@description Java script function that is called when 'submit' button is clicked
     function makeGrid() {
-
       //@description If there is already Grid created, remove it and create new one.
       $('tr').remove();
 
@@ -22,6 +21,20 @@
           $('tr').last().append('<td></td>');
         }
       }
+
+      //@description On click of any of the cells, take the valkue of color picker and change the backgrund of that cell
+      //@param {event lsitener object} e
+      $('table').on('mousedown', 'td', function(e) {
+
+        //@description Take the input of color picker
+        let colorChoosed = $('#colorPicker').val();
+
+        //@description Ge the cell that was clicked
+        let cellClicked = $(e.target);
+
+        //@description Apply background
+        cellClicked.css('background', colorChoosed);
+      });
     }
 
     //@description Get the form and on click of 'submit' button call makeGrid() function.
