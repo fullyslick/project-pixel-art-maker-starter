7d4eec8276929cfce6b4524b3a69eedabe200079
default <default@mail.com>
2018-01-16 13:08:28 +0200

    Takes the values of the input

--- designs.js
+++ designs.js
@@ -1,13 +1,19 @@
-// Select color input
-// Select size input
+// Take the dom elements and set them as constants
+const submitForm = document.getElementById('sizePicker');
+const canvas = document.getElementById('pixel_canvas');
 
-// When size is submitted by the user, call makeGrid()
-
+// @description The function that will build the grid
 function makeGrid() {
-
-// Your code goes here!
-alert("Hello World");
+  // @description Take the values of the input fields
+  let gridHeight = document.getElementById('input_height').value;
+  let gridWidth = document.getElementById('input_width').value;
+  alert(gridHeight + " " + gridWidth);
 }
 
-
-makeGrid();
+// @description Call make grid function on click of submit button
+submitForm.addEventListener("submit", function(event) {
+  // @description Prevent the default behaviur of submit button
+  event.preventDefault();
+  // @description Create the grid
+  makeGrid();
+});
