df843621669a153304590e5ec9f97dcc97864a9e
default <default@mail.com>
2018-01-16 13:36:06 +0200

    Removes previously created grid

--- designs.js
+++ designs.js
@@ -5,9 +5,24 @@ const canvas = document.getElementById('pixel_canvas');
 // @description The function that will build the grid
 function makeGrid() {
   // @description Take the values of the input fields
-  let gridHeight = document.getElementById('input_height').value;
-  let gridWidth = document.getElementById('input_width').value;
-  alert(gridHeight + " " + gridWidth);
+  let rows = document.getElementById('input_height').value;
+  let columns = document.getElementById('input_width').value;
+
+  // @description If there is already Grid created, remove it and create new one
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+
+  for (var r = 0; r < rows; r++) {
+    // @description Create table row element.
+    canvas.appendChild(document.createElement('tr'));
+    for (var c = 0; c < columns; c++) {
+      // @description Find the current row element to create table cell element inside it
+      let currentRow = document.getElementsByTagName('tr')[r];
+      //@description Create table cell.
+      currentRow.appendChild(document.createElement('td'));
+    }
+  }
 }
 
 // @description Call make grid function on click of submit button
